184df288315c38293434be54a88d1a7a3c13f192
Nick Saunders <nick.saunders@crosscutshredding.co.uk>
2024-10-21 12:18:20 +0100

    Add new blueprint files, home.html, and updated app.py

--- blueprints/auth.py
+++ blueprints/auth.py
@@ -2,7 +2,8 @@ from flask import Blueprint, render_template, redirect, url_for, request, flash
 from flask_login import login_user, logout_user, login_required
 from werkzeug.security import generate_password_hash, check_password_hash
 from app import db, login_manager
-from app.models import User  # Assuming you have a models.py with User defined
+from app import User
+
 
 # Create a blueprint for authentication
 auth_blueprint = Blueprint('auth', __name__)
