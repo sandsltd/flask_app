6230a0a47a8c773183c32aa5fbda795d4eba4dd7
Nick Saunders <nick.saunders@crosscutshredding.co.uk>
2024-11-01 15:21:39 +0000

    Add new blueprint files, home.html, and updated app.py

--- app.py
+++ app.py
@@ -903,7 +903,7 @@ def purchase(event_id):
                                 'currency': 'gbp',
                                 'product_data': {
                                     'name': 'Booking Fee',
-                                    'description': 'Includes platform, transaction, and Stripe fees',
+                                    'description': 'Includes platform, transaction, card charges',
                                 },
                                 'unit_amount': booking_fee_pence,
                             },
